2e1f5ffb1954b13a8d990fdd1ab032a5afa0f8e5
iAhmadMahmoud <ahmee7119@gmail.com>
2026-09-01 03:14:37 +0300

    Add EF Core models and context for products & categories
    Introduced `Category` and `Product` model classes with navigation properties. Implemented `AppDbContext` to configure relationships and constraints. Updated project file to remove explicit folder entries for Common, Data, and Models.

---
+++ ProductsCacheDemo/Data/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsCacheDemo.Models;
+
+namespace ProductsCacheDemo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {  }
+
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Product> Products => Set<Product>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+                entity.HasMany(c => c.Products)
+                .WithOne(p => p.Category)
+                .HasForeignKey(p=>p.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+                entity.Property(p => p.Price)
+                .HasPrecision(18, 2);
+                entity.Property(p => p.Description)
+                .HasMaxLength(500);
+            });
+        }
+    }
+}
---
+++ ProductsCacheDemo/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ProductsCacheDemo.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
